9805e3176112e92e304c621618d9bf60d5184ca9
VEXG <helmiaryanto54@gmail.com>
2021-11-10 23:53:53 +0700

    rename

---
+++ python/web-binar/reverse_array.py
@@ -0,0 +1,3 @@
+numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+kebalikan_list = list(reversed(numbers))
+print(kebalikan_list)
---
+++ python/web-binar/tabel_perkalian.py
@@ -0,0 +1,3 @@
+for a in range(1, 11):
+    for i in range(1, 11):
+        print(f'{a} X {i} = {a*i}')
